e69bfe7880e68b5440a6bff18009a7d8c4eb893b
AgusElmer <81542382+AgusElmer@users.noreply.github.com>
2021-04-23 13:33:34 -0300

    Adding Database

---
+++ Tables/dbo.Post.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Post] (
+  [Id] [int] IDENTITY,
+  [Title] [nvarchar](max) NULL,
+  [Content] [nvarchar](max) NULL,
+  [Image] [nvarchar](max) NULL,
+  [Category] [nvarchar](max) NULL,
+  [CreationDate] [datetime2] NOT NULL,
+  CONSTRAINT [PK_Post] PRIMARY KEY CLUSTERED ([Id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
